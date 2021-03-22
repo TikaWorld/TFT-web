2003b13889833125caf7272b21ee4cafb1803859
TikaWorld <tikaworld0416@gmail.com>
2021-03-23 01:58:07 +0900

    feat: add field cell component

--- src/battle/field.js
+++ src/battle/field.js
@@ -1,20 +1,45 @@
 import * as React from 'react';
+import styled from 'styled-components'
+
+const Hexa = styled.div` 
+  display: inline-block;
+  width: 100px;
+  height: 55px;
+  background: pink;
+  position: relative;
+  
+  &::before {
+      content: "";
+      position: absolute;
+      top: -25px;
+      left: 0;
+      width: 0;
+      height: 0;
+      border-left: 50px solid transparent;
+      border-right: 50px solid transparent;
+      border-bottom: 25px solid pink;
+    }
+  &::after {
+      content: "";
+      position: absolute;
+      bottom: -25px;
+      left: 0;
+      width: 0;
+      height: 0;
+      border-left: 50px solid transparent;
+      border-right: 50px solid transparent;
+      border-top: 25px solid pink;
+  }
+`
 
 export class Cell extends React.Component {
   constructor(props){
     super(props);
   }
   render(){
-    const style ={
-      width:"100px",
-      border: '2px solid #000',
-      height:"100px",
-      display:"inline-block"
-    };
-    
     return(
-        <div style ={style}>
-       </div>
+        <Hexa>
+       </Hexa>
     )
   }  
 }
@@ -26,23 +51,41 @@ export class Field extends React.Component {
   render(){
     const style ={
       position: "absolute",
-      width:"1000px",
+      width:"700px",
       height:"1000px",
+      top: "70%",
+      left: "50%",
+      transform: "translate(-50%, -50%)"
     };
+    const getCellRow = (i) => {
+      const isOdd = i%2
+      console.log(isOdd, i)
+      let style = {paddingTop: "25px", width:"700px", height:"50px"}
+      if (isOdd){
+        style = {paddingTop: "25px", paddingLeft: "50px", width:"700px", height:"50px"}
+      }
+
+      return <div style={style}>
+        <Cell id={1+i*7}/>
+        <Cell id={2+i*7}/>
+        <Cell id={3+i*7}/>
+        <Cell id={4+i*7}/>
+        <Cell id={5+i*7}/>
+        <Cell id={6+i*7}/>
+        <Cell id={7+i*7}/>
+      </div>
+    }
     
     return(
         <div style ={style}>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
+          {getCellRow(0)}  
+          {getCellRow(1)}  
+          {getCellRow(2)}  
+          {getCellRow(3)}  
+          {getCellRow(4)}  
+          {getCellRow(5)}  
+          {getCellRow(6)}  
+          {getCellRow(7)}  
        </div>
     )
   }  
