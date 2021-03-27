5af0fb671b5114fda01bd25449b44b7d3268bd46
TikaWorld <tikaworld0416@gmail.com>
2021-03-28 01:49:16 +0900

    feat: add static image, load champ img

--- src/component/stat.js
+++ src/component/stat.js
@@ -5,7 +5,6 @@ const BarContainer = styled.div`
   position: absolute;
   bottom: 0;
 `
-
 const Bar = styled.div` 
   width: 100px;
   height: 8px;
@@ -18,14 +17,22 @@ const Hp = styled(Bar)`
 const Mp = styled(Bar)` 
   background: blue;
 `
+const ChampImg = styled.img` 
+  position: absolute;
+  top: -5px;
+  left: 17.5px;
+`
 
 export default function Stat(props) {
   const data = props.data;
   const stat = data.stat;
   return(
+    <div>
+    <ChampImg src={"/champions/"+data.id+".png"}/> 
     <BarContainer>
       <Bar><Hp style={{width: (data.hp/stat.max_hp*100).toString()+"%"}}>{data.hp}</Hp></Bar>
       <Bar><Mp style={{width: (data.mp/stat.max_mp*100).toString()+"%"}}>{data.mp}</Mp></Bar> 
     </BarContainer>
+    </div>
   )
 }
